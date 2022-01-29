ba17a5c2b9876ece2cc7b82f7141f7c901a8d254
musleh0001 <khan.muslehuddin0001@gamil.com>
2022-01-29 14:16:41 +0600

    modified:   socialmedia/settings.py

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -109,6 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
